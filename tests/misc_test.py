9bc702ffe18edcf78d2302bbb13b74667cc12472
Jan Britz <britz@tu-berlin.de>
2023-01-10 11:38:13 +0100

    test: use variable instead of large numbers

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -5,12 +5,18 @@ import pytest
 from questionpy_common.misc import Bytes, ByteSize
 
 
+KB = 1024
+MB = 1024**2
+GB = 1024**3
+TB = 1024**4
+
+
 @pytest.mark.parametrize('byte_size, expected', [
     (ByteSize.B, 1),
-    (ByteSize.KiB, 1024),
-    (ByteSize.MiB, 1048576),
-    (ByteSize.GiB, 1073741824),
-    (ByteSize.TiB, 1099511627776),
+    (ByteSize.KiB, KB),
+    (ByteSize.MiB, MB),
+    (ByteSize.GiB, GB),
+    (ByteSize.TiB, TB),
 ])
 def test_byte_size(byte_size: ByteSize, expected: int) -> None:
     assert byte_size == expected
@@ -19,12 +25,12 @@ def test_byte_size(byte_size: ByteSize, expected: int) -> None:
 @pytest.mark.parametrize('value, unit, expected', [
     # Integers.
     (1, ByteSize.B, 1),
-    (1024, ByteSize.B, 1024),
-    (1, ByteSize.KiB, 1024),
-    (1, ByteSize.MiB, 1048576),
-    (1, ByteSize.GiB, 1073741824),
-    (1, ByteSize.TiB, 1099511627776),
-    (-1, ByteSize.KiB, -1024),
+    (1024, ByteSize.B, KB),
+    (1, ByteSize.KiB, KB),
+    (1, ByteSize.MiB, MB),
+    (1, ByteSize.GiB, GB),
+    (1, ByteSize.TiB, TB),
+    (-1, ByteSize.KiB, -KB),
 
     # Floats.
     (0.4, ByteSize.B, 0),
@@ -40,31 +46,44 @@ def test_byte_size(byte_size: ByteSize, expected: int) -> None:
     ('0.4', ByteSize.B, 0),
     ('0.6', ByteSize.B, 1),
     ('1.5', ByteSize.KiB, 1536),
-    ('1', ByteSize.MiB, 1048576),
-    ('1', ByteSize.GiB, 1073741824),
-    ('1', ByteSize.TiB, 1099511627776),
+    ('1', ByteSize.MiB, MB),
+    ('1', ByteSize.GiB, GB),
+    ('1', ByteSize.TiB, TB),
 
     ('+0.4', ByteSize.B, 0),
     ('+0.6', ByteSize.B, 1),
     ('+1.5', ByteSize.KiB, 1536),
-    ('+1', ByteSize.MiB, 1048576),
-    ('+1', ByteSize.GiB, 1073741824),
-    ('+1', ByteSize.TiB, 1099511627776),
+    ('+1', ByteSize.MiB, MB),
+    ('+1', ByteSize.GiB, GB),
+    ('+1', ByteSize.TiB, TB),
 
     ('-0.4', ByteSize.B, 0),
     ('-0.6', ByteSize.B, -1),
     ('-1.5', ByteSize.KiB, -1536),
-    ('-1', ByteSize.MiB, -1048576),
-    ('-1', ByteSize.GiB, -1073741824),
-    ('-1', ByteSize.TiB, -1099511627776),
+    ('-1', ByteSize.MiB, -MB),
+    ('-1', ByteSize.GiB, -GB),
+    ('-1', ByteSize.TiB, -TB),
 ])
 def test_init(value: float, unit: ByteSize, expected: int) -> None:
     assert Bytes(value, unit) == expected
 
 
-def test_zero() -> None:
-    assert 0 == Bytes(0) == Bytes(-0) == Bytes(0.0) == Bytes(-0.0) == Bytes('0') == Bytes('-0') == Bytes('+0') == \
-           Bytes('0.0') == Bytes('-0.0') == Bytes('+0.0')
+@pytest.mark.parametrize('value', [
+    Bytes(0),
+    Bytes(-0),
+    Bytes(+0),
+    Bytes(0.0),
+    Bytes(-0.0),
+    Bytes(+0.0),
+    Bytes('0'),
+    Bytes('-0'),
+    Bytes('+0'),
+    Bytes('0.0'),
+    Bytes('-0.0'),
+    Bytes('+0.0')
+])
+def test_zero(value: Bytes) -> None:
+    assert 0 == value
 
 
 def test_incorrect_type() -> None:
@@ -99,29 +118,29 @@ def test_incorrect_input(value: str) -> None:
     ('1.0', 1),
     ('1.4', 1),
     ('1.6', 2),
-    ('1024', 1024),
-    ('1024.0', 1024),
-    ('1024.5', 1024),
-    ('1024.6', 1025),
+    ('1024', KB),
+    ('1024.0', KB),
+    ('1024.5', KB),
+    ('1024.6', KB + 1),
 
     # With unit.
     ('1 b', 1),
     ('1 B', 1),
-    ('1k', 1024),
-    ('1kb', 1024),
-    ('1KB', 1024),
-    ('1kib', 1024),
-    ('1 m', 1048576),
-    ('1 mb', 1048576),
-    ('1 MB', 1048576),
-    ('1 mib', 1048576),
-    ('1 G', 1073741824),
-    ('1 Gb', 1073741824),
-    ('1 gB', 1073741824),
-    ('1 giB', 1073741824),
-    ('1 T  ', 1099511627776),
-    ('  1 tib', 1099511627776),
-    ('  1 TiB  ', 1099511627776),
+    ('1k', KB),
+    ('1kb', KB),
+    ('1KB', KB),
+    ('1kib', KB),
+    ('1 m', MB),
+    ('1 mb', MB),
+    ('1 MB', MB),
+    ('1 mib', MB),
+    ('1 G', GB),
+    ('1 Gb', GB),
+    ('1 gB', GB),
+    ('1 giB', GB),
+    ('1 T  ', TB),
+    ('  1 tib', TB),
+    ('  1 TiB  ', TB),
 ])
 def test_from_string(string: str, expected: int) -> None:
     assert Bytes.from_str(string) == expected
@@ -144,25 +163,25 @@ def test_from_string_not_valid(string: str) -> None:
     (Bytes(0), ByteSize.B, 0),
     (Bytes(-1), ByteSize.B, -1),
 
-    (Bytes(1024), ByteSize.KiB, 1),
-    (Bytes(1, ByteSize.KiB), ByteSize.B, 1024),
+    (Bytes(KB), ByteSize.KiB, 1),
+    (Bytes(1, ByteSize.KiB), ByteSize.B, KB),
 
-    (Bytes(1024, ByteSize.MiB), ByteSize.GiB, 1),
-    (Bytes(1, ByteSize.GiB), ByteSize.MiB, 1024),
+    (Bytes(KB, ByteSize.MiB), ByteSize.GiB, 1),
+    (Bytes(1, ByteSize.GiB), ByteSize.MiB, KB),
 
-    (Bytes(1024, ByteSize.MiB), ByteSize.GiB, 1),
-    (Bytes(1, ByteSize.GiB), ByteSize.MiB, 1024),
+    (Bytes(KB, ByteSize.MiB), ByteSize.GiB, 1),
+    (Bytes(1, ByteSize.GiB), ByteSize.MiB, KB),
 
-    (Bytes(1024, ByteSize.GiB), ByteSize.TiB, 1),
-    (Bytes(1, ByteSize.TiB), ByteSize.GiB, 1024),
+    (Bytes(KB, ByteSize.GiB), ByteSize.TiB, 1),
+    (Bytes(1, ByteSize.TiB), ByteSize.GiB, KB),
 
-    (Bytes(1024, ByteSize.TiB), ByteSize.GiB, 1024),
+    (Bytes(KB, ByteSize.TiB), ByteSize.GiB, KB),
 
     (Bytes(1536, ByteSize.KiB), ByteSize.GiB, 1.5),
     (Bytes(-1.5, ByteSize.GiB), ByteSize.KiB, -1536),
 
-    (Bytes(2, ByteSize.MiB), ByteSize.B, 2097152),
-    (Bytes(-2, ByteSize.GiB), ByteSize.B, -2147483648),
+    (Bytes(2, ByteSize.MiB), ByteSize.B, 2*MB),
+    (Bytes(-2, ByteSize.GiB), ByteSize.B, -2*MB),
 ])
 def test_convert_to(value: Bytes, unit: ByteSize, expected: int) -> None:
     pytest.approx(value.convert_to(unit), expected)
@@ -170,11 +189,11 @@ def test_convert_to(value: Bytes, unit: ByteSize, expected: int) -> None:
 
 @pytest.mark.parametrize('value, expected', [
     (Bytes(1), '1 B'),
-    (Bytes(1024), '1.00 KiB'),
-    (Bytes(1024, ByteSize.KiB), '1.00 MiB'),
-    (Bytes(1024, ByteSize.MiB), '1.00 GiB'),
-    (Bytes(1024, ByteSize.GiB), '1.00 TiB'),
-    (Bytes(1024, ByteSize.TiB), '1024.00 TiB'),
+    (Bytes(KB), '1.00 KiB'),
+    (Bytes(KB, ByteSize.KiB), '1.00 MiB'),
+    (Bytes(KB, ByteSize.MiB), '1.00 GiB'),
+    (Bytes(KB, ByteSize.GiB), '1.00 TiB'),
+    (Bytes(KB, ByteSize.TiB), '1024.00 TiB'),
 
     (Bytes(1.5), '2 B'),
     (Bytes(1536), '1.50 KiB'),
