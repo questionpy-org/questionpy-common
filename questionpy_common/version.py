c24653e04c4c70ea325b4a3355c639cc29933bdc
Jan Britz <britz@tu-berlin.de>
2023-03-28 11:15:12 +0200

    feat: replace `semver` with own implementation

--- questionpy_common/version.py
+++ questionpy_common/version.py
@@ -1,59 +1,152 @@
 import re
 from functools import total_ordering
-from typing import Callable, Iterator
-
-from semver import VersionInfo  # type: ignore
-
-
-class SemVer(VersionInfo):
-    @classmethod
-    def _parse(cls, version: str) -> 'SemVer':
-        return cls.parse(version)
-
-    @classmethod
-    def __get_validators__(cls) -> Iterator[Callable[[str], 'SemVer']]:
-        yield cls._parse
-
-
-RE_API = re.compile(r'^(?P<major>\d+)\.(?P<minor>\d+)$')
+from typing import Callable, Iterator, Optional
 
 
 @total_ordering
-class APIVersion:
+class _ComparableVersion:
+    """
+    A multipart version that can be compared to other versions.
+    """
+    def __init__(self, *parts: int):
+        for part in parts:
+            if part < 0:
+                raise ValueError("negative version parts are not allowed")
+
+        self._length = len(parts)
+        self._parts = parts
+
+    def _lt(self, other: '_ComparableVersion', pos: int = 0) -> bool:
+        if self._parts[pos] < other._parts[pos]:
+            return True
+        if self._parts[pos] == other._parts[pos] and pos < self._length - 1:
+            return self._lt(other, pos + 1)
+        return False
+
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, _ComparableVersion) or self._length != other._length:
+            raise NotImplementedError()
+        return self._lt(other)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, _ComparableVersion):
+            raise NotImplementedError()
+        return self._parts == other._parts
+
+
+class APIVersion(_ComparableVersion):
+    """
+    Represents an API version consisting of a major and minor version number.
+    """
+
+    _RE_API = re.compile(r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)$')
+
     def __init__(self, major: int, minor: int = 0):
-        if major < 0 or minor < 0:
-            raise ValueError("each number of the version must be zero or positive")
-        self.major = major
-        self.minor = minor
+        super().__init__(major, minor)
+
+        self._str: Optional[str] = None
+        self._repr: Optional[str] = None
+
+    @property
+    def major(self) -> int:
+        return self._parts[0]
+
+    @property
+    def minor(self) -> int:
+        return self._parts[1]
 
     @classmethod
-    def _parse(cls, version: str) -> 'APIVersion':
-        if not (match := RE_API.match(version)):
-            raise ValueError(f"'{version}' is not valid")
+    def from_string(cls, version: str) -> 'APIVersion':
+        if not (match := cls._RE_API.match(version)):
+            raise ValueError("invalid API version string")
+
         major = int(match.group('major'))
         minor = int(match.group('minor'))
+
         return cls(major, minor)
 
     @classmethod
     def __get_validators__(cls) -> Iterator[Callable[[str], 'APIVersion']]:
-        yield cls._parse
+        yield cls.from_string
 
     def __str__(self) -> str:
-        return f'{self.major}.{self.minor}'
+        if self._str is None:
+            self._str = f'{self.major}.{self.minor}'
+        return self._str
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}(major={self.major}, minor={self.minor})'
 
-    def __lt__(self, other: object) -> bool:
-        if not isinstance(other, APIVersion):
-            raise NotImplementedError()
-        if self.major < other.major:
-            return True
-        if self.major == other.major:
-            return self.minor < other.minor
-        return False
 
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, APIVersion):
-            raise NotImplementedError()
-        return (self.major, self.minor) == (other.major, other.minor)
+class SemVer(_ComparableVersion):
+    """
+    Represents a semantic version consisting of a major, minor and patch version number. Optionally, a prerelease and
+    build version can be specified.
+    """
+    _RE_SEMVER = re.compile(r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)'
+                            r'(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)'
+                            r'(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?'
+                            r'(?:\+(?P<build>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$')
+
+    def __init__(self, major: int, minor: int = 0, patch: int = 0, prerelease: Optional[str] = None,
+                 build: Optional[str] = None):
+        super().__init__(major, minor, patch)
+
+        self._prerelease = prerelease
+        self._build = build
+
+        self._str: Optional[str] = None
+        self._repr: Optional[str] = None
+
+    @property
+    def major(self) -> int:
+        return self._parts[0]
+
+    @property
+    def minor(self) -> int:
+        return self._parts[1]
+
+    @property
+    def patch(self) -> int:
+        return self._parts[2]
+
+    @property
+    def prerelease(self) -> Optional[str]:
+        return self._prerelease
+
+    @property
+    def build(self) -> Optional[str]:
+        return self._build
+
+    @classmethod
+    def from_string(cls, version: str) -> 'SemVer':
+        if not (match := cls._RE_SEMVER.match(version)):
+            raise ValueError("invalid SemVer string")
+
+        major = int(match.group('major'))
+        minor = int(match.group('minor'))
+        patch = int(match.group('patch'))
+
+        prerelease = match.group('prerelease')
+        build = match.group('build')
+
+        return cls(major, minor, patch, prerelease, build)
+
+    @classmethod
+    def __get_validators__(cls) -> Iterator[Callable[[str], 'SemVer']]:
+        yield cls.from_string
+
+    def __str__(self) -> str:
+        if self._str is None:
+            self._str = f'{self.major}.{self.minor}.{self.patch}'
+            if self.prerelease:
+                self._str += f'-{self.prerelease}'
+            if self.build:
+                self._str += f'+{self.build}'
+        return self._str
+
+    def __repr__(self) -> str:
+        if self._repr is None:
+            self._repr = f'{self.__class__.__name__}(major={self.major}, minor={self.minor}, patch={self.patch},' \
+                         f'prerelease={self.prerelease}, build={self.build})'
+        return self._repr
--- tests/version_test.py
+++ tests/version_test.py
@@ -1,147 +1,269 @@
-# pylint: disable=protected-access, unneeded-not
+# pylint: disable=unneeded-not
 
-from typing import Optional
+from typing import Optional, NamedTuple
 
 import pytest
-from pydantic import BaseModel, parse_raw_as
 
 from questionpy_common.version import SemVer, APIVersion
 
 
-def test_use_semver_in_basemodel() -> None:
-    class Model(BaseModel):
-        version: SemVer
-
-        class Config:
-            json_encoders = {
-                SemVer: str,
-            }
-
-    model_1 = Model(version='1.0.0')
-    json = model_1.json()
-    model_2 = parse_raw_as(Model, json)
-
-    assert model_1 == model_2
+class V(NamedTuple):
+    major: int
+    minor: int = 0
+    patch: int = 0
+    prerelease: Optional[str] = None
+    build: Optional[str] = None
 
 
-def test_use_api_version_in_basemodel() -> None:
-    class Model(BaseModel):
-        version: APIVersion
+valid_semver: dict[str, V] = {
+    '0.0.4': V(0, 0, 4, None, None),
+    '1.2.3': V(1, 2, 3, None, None),
+    '10.20.30': V(10, 20, 30, None, None),
+    '1.1.2-prerelease+meta': V(1, 1, 2, 'prerelease', 'meta'),
+    '1.1.2+meta': V(1, 1, 2, None, 'meta'),
+    '1.1.2+meta-valid': V(1, 1, 2, None, 'meta-valid'),
+    '1.0.0-alpha': V(1, 0, 0, 'alpha', None),
+    '1.0.0-beta': V(1, 0, 0, 'beta', None),
+    '1.0.0-alpha.beta': V(1, 0, 0, 'alpha.beta', None),
+    '1.0.0-alpha.beta.1': V(1, 0, 0, 'alpha.beta.1', None),
+    '1.0.0-alpha.1': V(1, 0, 0, 'alpha.1', None),
+    '1.0.0-alpha0.valid': V(1, 0, 0, 'alpha0.valid', None),
+    '1.0.0-alpha.0valid': V(1, 0, 0, 'alpha.0valid', None),
+    '1.0.0-alpha-a.b-c-somethinglong+build.1-aef.1-its-okay': V(1, 0, 0, 'alpha-a.b-c-somethinglong',
+                                                                'build.1-aef.1-its-okay'),
+    '1.0.0-rc.1+build.1': V(1, 0, 0, 'rc.1', 'build.1'),
+    '2.0.0-rc.1+build.123': V(2, 0, 0, 'rc.1', 'build.123'),
+    '1.2.3-beta': V(1, 2, 3, 'beta', None),
+    '10.2.3-DEV-SNAPSHOT': V(10, 2, 3, 'DEV-SNAPSHOT', None),
+    '1.2.3-SNAPSHOT-123': V(1, 2, 3, 'SNAPSHOT-123', None),
+    '1.0.0': V(1, 0, 0, None, None),
+    '2.0.0': V(2, 0, 0, None, None),
+    '1.1.7': V(1, 1, 7, None, None),
+    '2.0.0+build.1848': V(2, 0, 0, None, 'build.1848'),
+    '2.0.1-alpha.1227': V(2, 0, 1, 'alpha.1227', None),
+    '1.0.0-alpha+beta': V(1, 0, 0, 'alpha', 'beta'),
+    '1.2.3----RC-SNAPSHOT.12.9.1--.12+788': V(1, 2, 3, '---RC-SNAPSHOT.12.9.1--.12', '788'),
+    '1.2.3----R-S.12.9.1--.12+meta': V(1, 2, 3, '---R-S.12.9.1--.12', 'meta'),
+    '1.2.3----RC-SNAPSHOT.12.9.1--.12': V(1, 2, 3, '---RC-SNAPSHOT.12.9.1--.12', None),
+    '1.0.0+0.build.1-rc.10000aaa-kk-0.1': V(1, 0, 0, None, '0.build.1-rc.10000aaa-kk-0.1'),
+    '99999999999999999999999.999999999999999999.99999999999999999': V(99999999999999999999999, 999999999999999999,
+                                                                      99999999999999999, None, None),
+    '1.0.0-0A.is.legal': V(1, 0, 0, '0A.is.legal', None)
+}
 
-        class Config:
-            json_encoders = {
-                APIVersion: str,
-            }
 
-    model_1 = Model(version="1.0")
-    json = model_1.json()
-    model_2 = parse_raw_as(Model, json)
+@pytest.mark.parametrize('string, components', valid_semver.items())
+def test_valid_semver_initialisation(string: str, components: V) -> None:
+    semver = SemVer(*components)
 
-    assert model_1 == model_2
+    assert semver.major == components.major
+    assert semver.minor == components.minor
+    assert semver.patch == components.patch
+    assert semver.prerelease == components.prerelease
+    assert semver.build == components.build
+
+    assert str(semver) == string
 
 
-@pytest.mark.parametrize('version', [
-    '0.0',
-    '0.1',
-    '1.0',
-    '42.21',
-    '00.00',
-    '010.010',
+@pytest.mark.parametrize('string, components', valid_semver.items())
+def test_valid_semver_string_parsing(string: str, components: V) -> None:
+    semver = SemVer.from_string(string)
+
+    assert semver.major == components.major
+    assert semver.minor == components.minor
+    assert semver.patch == components.patch
+    assert semver.prerelease == components.prerelease
+    assert semver.build == components.build
+
+    assert str(semver) == string
+
+
+@pytest.mark.parametrize('string', [
+    '1',
+    '1.2',
+    '1.2.3-0123',
+    '1.2.3-0123.0123',
+    '1.1.2+.123',
+    '+invalid',
+    '-invalid',
+    '-invalid+invalid',
+    '-invalid.01',
+    'alpha',
+    'alpha.beta',
+    'alpha.beta.1',
+    'alpha.1',
+    'alpha+beta',
+    'alpha_beta',
+    'alpha.',
+    'alpha..',
+    'beta',
+    '1.0.0-alpha_beta',
+    '-alpha.',
+    '1.0.0-alpha..',
+    '1.0.0-alpha..1',
+    '1.0.0-alpha...1',
+    '1.0.0-alpha....1',
+    '1.0.0-alpha.....1',
+    '1.0.0-alpha......1',
+    '1.0.0-alpha.......1',
+    '01.1.1',
+    '1.01.1',
+    '1.1.01',
+    '1.2',
+    '1.2.3.DEV',
+    '1.2-SNAPSHOT',
+    '1.2.31.2.3----RC-SNAPSHOT.12.09.1--..12+788',
+    '1.2-RC-SNAPSHOT',
+    '-1.0.3-gamma+b7718',
+    '+justmeta',
+    '9.8.7+meta+meta',
+    '9.8.7-whatever+meta+meta',
+    '99999999999999999999999.999999999999999999.99999999999999999----RC-SNAPSHOT.12.09.1-------------------------..12'
 ])
-def test_parse_api_version_successfully(version: str) -> None:
-    APIVersion._parse(version)
+def test_invalid_semver_string_parsing(string: str) -> None:
+    with pytest.raises(ValueError, match='invalid SemVer string'):
+        SemVer.from_string(string)
 
 
-@pytest.mark.parametrize('version', [
+@pytest.mark.parametrize('components', [
+    (-1,),
+    (0, -1),
+    (0, 0, -1)
+])
+def test_semver_initialisation_with_negative_values_raise_error(components: tuple) -> None:
+    with pytest.raises(ValueError, match='negative version parts are not allowed'):
+        SemVer(*components)
+
+
+@pytest.mark.parametrize('components', valid_semver.values())
+def test_semver_equality_check(components: V) -> None:
+    semver_1 = SemVer(*components)
+    semver_2 = SemVer(*components)
+
+    assert semver_1 == semver_2
+    assert semver_1 >= semver_2
+    assert semver_1 <= semver_2
+    assert not semver_1 != semver_2
+    assert not semver_1 > semver_2
+    assert not semver_1 < semver_2
+
+
+@pytest.mark.parametrize('version_1, version_2', [
+    ('1.0.0', '1.1.1'),
+    ('1.0.0', '1.1.0'),
+    ('1.0.0', '1.1.1'),
+    ('1.0.0', '2.0.0'),
+    ('1.0.9', '1.0.10'),
+    ('1.9.0', '1.10.0'),
+    ('1.9.0', '2.0.0')
+])
+def test_semver_inequality_check(version_1: str, version_2: str) -> None:
+    semver_1 = SemVer.from_string(version_1)
+    semver_2 = SemVer.from_string(version_2)
+
+    assert semver_1 != semver_2
+    assert semver_1 < semver_2
+    assert semver_1 <= semver_2
+    assert not semver_1 == semver_2
+    assert not semver_1 > semver_2
+    assert not semver_1 >= semver_2
+
+    assert semver_2 != semver_1
+    assert semver_2 > semver_1
+    assert semver_2 >= semver_1
+    assert not semver_2 == semver_1
+    assert not semver_2 < semver_1
+    assert not semver_2 <= semver_1
+
+
+valid_api_versions = {
+    '0.0': V(0, 0),
+    '0.1': V(0, 1),
+    '1.0': V(1, 0),
+    '42.21': V(42, 21),
+    '10.10': V(10, 10),
+    '9999999999999.99999999999': V(9999999999999, 99999999999)
+}
+
+
+@pytest.mark.parametrize('string, components', valid_api_versions.items())
+def test_valid_api_version_initialisation(string: str, components: V) -> None:
+    api = APIVersion(*components[:2])
+
+    assert api.major == components.major
+    assert api.minor == components.minor
+
+    assert str(api) == string
+
+
+@pytest.mark.parametrize('string, components', valid_api_versions.items())
+def test_valid_api_version_string_parsing(string: str, components: V) -> None:
+    api = APIVersion(*components[:2])
+
+    assert api.major == components.major
+    assert api.minor == components.minor
+
+    assert str(api) == string
+
+
+@pytest.mark.parametrize('string', [
     '-0.0',
     '0.-0',
     '0.0-',
     '0.0.0',
     '42',
     '1 0',
+    'invalid'
 ])
-def test_parse_api_version_unsuccessfully(version: str) -> None:
-    with pytest.raises(ValueError):
-        APIVersion._parse(version)
+def test_invalid_api_version_string_parsing(string: str) -> None:
+    with pytest.raises(ValueError, match='invalid API version string'):
+        APIVersion.from_string(string)
 
 
-@pytest.mark.parametrize('v_1, v_2', [
+@pytest.mark.parametrize('components', [
+    (-1,),
+    (0, -1),
+])
+def test_api_version_initialisation_with_negative_values_raise_error(components: tuple) -> None:
+    with pytest.raises(ValueError, match='negative version parts are not allowed'):
+        APIVersion(*components)
+
+
+@pytest.mark.parametrize('components', valid_api_versions.values())
+def test_api_version_equality_check(components: V) -> None:
+    api_components = components[:2]
+    version_1 = APIVersion(*api_components)
+    version_2 = APIVersion(*api_components)
+
+    assert version_1 == version_2
+    assert version_1 >= version_2
+    assert version_1 <= version_2
+    assert not version_1 != version_2
+    assert not version_1 > version_2
+    assert not version_1 < version_2
+
+
+@pytest.mark.parametrize('version_1, version_2', [
     ('0.0', '0.1'),
     ('0.1', '1.0'),
     ('1.0', '1.1'),
     ('1.0', '2.0'),
     ('11.0', '12.0'),
 ])
-def test_compare_api_version_inequality(v_1: str, v_2: str) -> None:
-    version_1 = APIVersion._parse(v_1)
-    version_2 = APIVersion._parse(v_2)
+def test_api_version_inequality_check(version_1: str, version_2: str) -> None:
+    api_1 = APIVersion.from_string(version_1)
+    api_2 = APIVersion.from_string(version_2)
 
-    assert version_1 < version_2
-    assert version_1 <= version_2
-    assert version_2 > version_1
-    assert version_2 >= version_1
+    assert api_1 != api_2
+    assert api_1 < api_2
+    assert api_1 <= api_2
+    assert not api_1 == api_2
+    assert not api_1 > api_2
+    assert not api_1 >= api_2
 
-
-@pytest.mark.parametrize('version', [
-    '0.0',
-    '1.0',
-    '12.0',
-    '0.12',
-    '12.12'
-])
-def test_compare_api_version_equality(version: str) -> None:
-    version_1 = APIVersion._parse(version)
-    version_2 = APIVersion._parse(version)
-
-    assert version_1 == version_2
-    assert version_1 <= version_2
-    assert version_1 >= version_2
-    assert not version_1 != version_2
-
-
-@pytest.mark.parametrize('v_1, v_2', [
-    ('0.0', '0.00'),
-    ('01.1', '1.1'),
-    ('001.100', '1.100'),
-    ('01.000', '0001.0')
-])
-def test_compare_special_api_version_equality(v_1: str, v_2: str) -> None:
-    version_1 = APIVersion._parse(v_1)
-    version_2 = APIVersion._parse(v_2)
-
-    assert version_1 == version_2
-    assert version_1 <= version_2
-    assert version_1 >= version_2
-    assert not version_1 != version_2
-
-
-@pytest.mark.parametrize('major, minor', [
-    (0, None),
-    (0, 0),
-    (1, None),
-    (1, 1),
-    (42, None),
-    (42, 0),
-    (42, 42)
-])
-def test_successful_initialisation(major: int, minor: Optional[int]) -> None:
-    if minor:
-        APIVersion(major, minor)
-    else:
-        APIVersion(major)
-
-
-@pytest.mark.parametrize('major, minor', [
-    (0, -1),
-    (-1, None),
-    (-1, 0),
-    (-1, -1),
-    (-42, 0),
-    (0, -42)
-])
-def test_unsuccessful_initialisation(major: int, minor: Optional[int]) -> None:
-    with pytest.raises(ValueError):
-        if minor:
-            APIVersion(major, minor)
-        else:
-            APIVersion(major)
+    assert api_2 != api_1
+    assert api_2 > api_1
+    assert api_2 >= api_1
+    assert not api_2 == api_1
+    assert not api_2 < api_1
+    assert not api_2 <= api_1
