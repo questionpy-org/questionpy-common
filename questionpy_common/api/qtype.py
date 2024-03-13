6f47fc205c767564baaee914e064f9ba152bae09
Maximilian Haye <m.haye@tu-berlin.de>
2024-03-13 17:46:30 +0100

    refactor: type hint form data as Mapping to allow covariance
    Fixes mypy warnings such as 'Incompatible return value type (got "tuple[OptionsFormDefinition, dict[str, dict[str, object]]]", expected "tuple[OptionsFormDefinition, dict[str, object]]")'.

--- questionpy_common/api/qtype.py
+++ questionpy_common/api/qtype.py
@@ -3,7 +3,7 @@
 #  (c) Technische Universität Berlin, innoCampus <info@isis.tu-berlin.de>
 
 from abc import ABC, abstractmethod
-from typing import Optional, TypeVar, Generic
+from typing import Optional, TypeVar, Generic, Mapping
 
 from .question import BaseQuestion
 from ..elements import OptionsFormDefinition
@@ -15,7 +15,7 @@ _Q = TypeVar("_Q", bound=BaseQuestion)
 
 class BaseQuestionType(ABC, Generic[_Q]):
     @abstractmethod
-    def get_options_form(self, question: Optional[_Q]) -> tuple[OptionsFormDefinition, dict[str, object]]:
+    def get_options_form(self, question: Optional[_Q]) -> tuple[OptionsFormDefinition, Mapping[str, object]]:
         """Get the form used to create a new or edit an existing question.
 
         Args:
@@ -26,7 +26,7 @@ class BaseQuestionType(ABC, Generic[_Q]):
         """
 
     @abstractmethod
-    def create_question_from_options(self, form_data: dict[str, object],
+    def create_question_from_options(self, form_data: Mapping[str, object],
                                      old_question: Optional[_Q]) -> _Q:
         """Create or update the question (state) with the form data from a submitted question edit form.
 
